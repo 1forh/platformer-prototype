42155279c8f051f9c629073da9210a5025d11769
1forh <1forhlol@gmail.com>
2020-05-16 22:16:59 -0400

    inits

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 moveDirection = new Vector3(horizontalInput, 0.0f, 0.0f);
+        moveDirection *= _speed;
+        _controller.Move(moveDirection * Time.deltaTime);
+    }
+}
